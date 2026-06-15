b1489e01e912f2e428a28927ff3a34c23652d2af
Sabal <sabalcs7@gmail.com>
2026-06-16 00:10:50 +0545

    Basic Duplicate File Finder

--- Duplicate_File_Finder/Duplicate_File_Finder.py
+++ Duplicate_File_Finder/Duplicate_File_Finder.py
@@ -3,6 +3,7 @@ import hashlib
 
 files_dir = {}  # "file_hash" = "file_name"
 directory = input("enter path of folder:")
+duplicate_file_count = 0
 
 for root, _, files in os.walk(directory):
     for file in files:
@@ -14,9 +15,14 @@ for root, _, files in os.walk(directory):
             continue
         file_hash = hashlib.md5(content).hexdigest()
         if file_hash in files_dir:
-            print("- Duplicate found!")
-            print("- Original=", files_dir[file_hash])
-            print("- Duplicate=", file)
+            print("--- Duplicate found! ---")
+            print("Original=", files_dir[file_hash])
+            print("Duplicate=", file)
+            duplicate_file_count += 1
         else:
-            files_dir[file_hash] = file_path
-print("Program exited!")
+            files_dir[file_hash] = file
+print("-------------------------------------")
+if duplicate_file_count == 0:
+    print("No duplicate file found!")
+else:
+    print(duplicate_file_count, "duplicate pairs found!")
