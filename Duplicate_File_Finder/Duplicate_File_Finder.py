722edd2d241b7e2640f5d01d2d399f5c2af9774e
Sabal <sabalcs7@gmail.com>
2026-06-15 23:56:50 +0545

    basic dupe file finder

--- Duplicate_File_Finder/Duplicate_File_Finder.py
+++ Duplicate_File_Finder/Duplicate_File_Finder.py
@@ -1,5 +1,22 @@
-import sys
 import os
-import shutil
+import hashlib
 
-print("PROJECT IN PROGRESS!")
+files_dir = {}  # "file_hash" = "file_name"
+directory = input("enter path of folder:")
+
+for root, _, files in os.walk(directory):
+    for file in files:
+        file_path = os.path.join(root, file)
+        try:
+            with open(file_path, "rb") as f:
+                content = f.read()
+        except (PermissionError, OSError):
+            continue
+        file_hash = hashlib.md5(content).hexdigest()
+        if file_hash in files_dir:
+            print("- Duplicate found!")
+            print("- Original=", files_dir[file_hash])
+            print("- Duplicate=", file)
+        else:
+            files_dir[file_hash] = file_path
+print("Program exited!")
